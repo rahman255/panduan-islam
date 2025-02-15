3e5f59fbbc4500e516ffbafb2d9fcedfc37774fb
alya fatmah daulay <154336686+rahman255@users.noreply.github.com>
2025-02-14 18:36:21 -0800

    Add files via upload

---
+++ doa/java.js
@@ -0,0 +1,15 @@
+{/* <script>
+    function searchDoa() {
+      const searchValue = document.getElementById('search').value.toLowerCase();
+      const cards = document.querySelectorAll('.doa-card');
+      
+      cards.forEach(card => {
+        const title = card.querySelector('.doa-title').innerText.toLowerCase();
+        if (title.includes(searchValue)) {
+          card.style.display = '';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    }
+  </script> */}
